dc949cd20ca3592404eaa46fcc20eec2d39cbe63
leesm <ask135799@naver.com>
2025-10-01 14:01:09 +0900

    타입스크립트 1일차 과제

---
+++ src/prac/index.tsx
@@ -0,0 +1,10 @@
+export const TypePractice1 = () => {
+
+    
+    return(
+        <>
+        <h1>Type Practice 1</h1>
+        </>
+    )
+
+}
---
+++ src/prac/typePrac.ts
@@ -0,0 +1,14 @@
+console.log("Hello");
+
+const num: number = 10;
+console.log(num);
+const str: string = "TS";
+console.log(str);
+const nums: number[] = [4, 5, 6];
+console.log(nums);
+const isBoolean: boolean = true;
+console.log(isBoolean);
+const nullValue: null = null;
+console.log(nullValue);
+const anyType: any = [1, "2", false];
+console.log(anyType);
